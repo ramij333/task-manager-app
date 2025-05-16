162a73ad0e99d322144fcb4611ab06334ea3ac1c
ramij333 <skramijsc@gmail.com>
2025-05-16 19:29:50 +0530

    Use environment variable for API base URL

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api",
   //credentials: "include"
   withCredentials: true,
   headers: {
